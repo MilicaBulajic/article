24ad0b3020143f0a74c4d8b533925c54aec7b445
Milica Bulajic <milicabulajic@Milicas-MacBook-Pro.local>
2022-03-13 20:16:31 +0100

    sequelize connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 //instance of express
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 const db = require("./models");
 
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("server running on port 3001");
+// Sync sequelize models then start Express app
+// =============================================
+db.sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log("\n*************************************");
+    console.log(`${process.env.DB_DATABASE} database connected`);
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on PORT ${PORT}`);
+      console.log("*************************************\n");
+    });
   });
-});
